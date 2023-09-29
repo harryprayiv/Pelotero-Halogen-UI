ac15d199f8595da6839257a7ea8a23b243254993
harryprayiv <harryprayiv@gmail.com>
2023-09-29 15:17:32 -0400

    can't even do simple stuff in Purescript!

--- src/Main.purs
+++ src/Main.purs
@@ -7,9 +7,10 @@ import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
 import Halogen.HTML as HH
 import Halogen.HTML.Properties as HP
+import Effect.Aff (Aff)
 
 type Player = 
-  { id :: Int
+  { playerId :: Int
   , name :: String
   , teamId :: Int
   , position :: String
@@ -17,45 +18,53 @@ type Player =
 
 playerData :: Array Player
 playerData = 
-  [ { id: 408234, name: "Miguel Cabrera", teamId: 116, position: "10" }
-  , { id: 425794, name: "Adam Wainwright", teamId: 138, position: "1" }
+  [ { playerId: 408234, name: "Miguel Cabrera", teamId: 116, position: "10" }
+  , { playerId: 425794, name: "Adam Wainwright", teamId: 138, position: "1" }
   -- ... other players
   ]
 
-ui :: forall query input output m. H.Component query input output m
+data Action = Initialize
+
+data Query a
+
+type State = { players :: Array Player }
+
+ui :: forall m. H.Component HH.HTML Query Unit m
 ui =
   H.mkComponent
-    { initialState: identity
+    { initialState: \_ -> { players: playerData }
     , render
-    , eval: H.mkEval H.defaultEval
+    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
     }
 
   where
-  playerRow :: Player -> HH.HTML m Unit
+  playerRow :: Player -> HH.HTML m Action
   playerRow player =
       HH.tr_
-        [ HH.td_ [ HH.text $ show player.id ]
+        [ HH.td_ [ HH.text $ show player.playerId ]
         , HH.td_ [ HH.text player.name ]
         , HH.td_ [ HH.text $ show player.teamId ]
         , HH.td_ [ HH.text player.position ]
         ]
 
-  render :: forall state. state -> H.ComponentHTML query input m
-  render _ =
+  render :: State -> HH.HTML m Action
+  render state =
       HH.div_
         [ HH.table_
             ( [ HH.thead_
                   [ HH.tr_
-                      [ HH.th_ [ HH.text "ID" ]
+                      [ HH.th_ [ HH.text "PlayerID" ]
                       , HH.th_ [ HH.text "Name" ]
                       , HH.th_ [ HH.text "Team ID" ]
                       , HH.th_ [ HH.text "Position" ]
                       ]
                   ]
-              ] <> map playerRow playerData
+              ] <> map playerRow state.players
             )
         ]
 
+  handleAction :: Action -> H.HalogenM State Query () m Unit
+  handleAction _ = pure unit
 
 main :: Effect Unit
 main =
