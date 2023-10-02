397577b924a027ff855008e31c0f200bbbb29a3c
harryprayiv <harryprayiv@gmail.com>
2023-10-02 15:21:01 -0400

    good starting point finally

--- src/Roster.purs
+++ src/Roster.purs
@@ -1,11 +1,10 @@
 module Roster where
 
-import Prelude
+-- import Prelude
 import Data.Argonaut.Decode.Class (class DecodeJson)
-import Data.Argonaut.Decode.Generic (class DecodeRep, genericDecodeJson)
+import Data.Argonaut.Decode.Generic (genericDecodeJson)
 import Data.Argonaut.Encode.Class (class EncodeJson)
 import Data.Argonaut.Encode.Generic (genericEncodeJson)
-import Data.Argonaut.Encode.Generic (class EncodeRep, genericEncodeJson)
 import Data.Generic.Rep (class Generic)
 import Data.Maybe (Maybe)
 
